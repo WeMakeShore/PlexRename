992444eb7365e3a6179e1b3847cd741bcecf6fdb
Murakami <ruble.timothy@gmail.com>
2021-08-04 03:57:59 -0500

    Base Changes made Pre-API involvement

--- PlexRename/Program.cs
+++ PlexRename/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PlexRename
 {
@@ -6,7 +7,34 @@ namespace PlexRename
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Media Renaming tool! Please input the information requested and the files will be updated shortly.");
+            Console.WriteLine("Enter Folder Path: ");
+            string dir = Console.ReadLine();
+            string[] files = Directory.GetFiles(dir);
+            Console.WriteLine("Enter Name of Show: ");
+            string mediaName = Console.ReadLine();
+            Console.WriteLine("Pick a Format:");
+            Console.WriteLine("1. " + mediaName + " - S00E00");
+            Console.WriteLine("2. " + mediaName + " - E00");
+            int format = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many seasons are in this location?");
+            int seasonCount = Convert.ToInt32(Console.ReadLine());
+            if (seasonCount > 1) {
+                Console.WriteLine("Are there the same amount of episodes each season? y/n");
+                string equalEpisodes = Console.ReadLine();
+                if(equalEpisodes[0] == 'y' || equalEpisodes[0] == 'Y') {
+                    Console.WriteLine("How many episodes are there in each season?");
+                    int episodesPerSeason = Convert.ToInt32(Console.ReadLine());
+                }
+                else { 
+                
+                }
+                    }
+
+            //Output Current Name and new Name
+            foreach (string file in files)
+                Console.WriteLine(Path.GetFileName(file));
         }
     }
 }
